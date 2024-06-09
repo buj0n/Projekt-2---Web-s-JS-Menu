ce998a103a80237cce17737f5a6da0f824496433
Tomas Bujansky <tomas.bujansky@gmail.com>
2024-06-09 20:17:52 +0200

    Přidána js logika pro klikání na ikonku hamburger menu.

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-// Opakující se věci
-// Nav + footer - zkusit generovat přes js?
+const menuIcon = document.querySelector('.menu-icon');
+const menuList = document.querySelector('nav');
+const hamburgerIcon = document.querySelector('.fa-solid');
+
+menuIcon.addEventListener('click', () => {
+  if (hamburgerIcon.classList[1] == 'fa-bars') {
+    hamburgerIcon.classList.add('fa-xmark');
+    hamburgerIcon.classList.remove('fa-bars');
+  } else {
+    hamburgerIcon.classList.add('fa-bars');
+    hamburgerIcon.classList.remove('fa-xmark');
+  }
+});
