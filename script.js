d3bfa619057c73e68bd79fc3e97cac5a679cbefe
Tomas Bujansky <tomas.bujansky@gmail.com>
2024-06-09 20:36:53 +0200

    Dodělaná logika js responzivního menu

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ menuIcon.addEventListener('click', () => {
   if (hamburgerIcon.classList[1] == 'fa-bars') {
     hamburgerIcon.classList.add('fa-xmark');
     hamburgerIcon.classList.remove('fa-bars');
+    menuList.style.display = 'block';
   } else {
     hamburgerIcon.classList.add('fa-bars');
     hamburgerIcon.classList.remove('fa-xmark');
+    menuList.style.display = 'none';
   }
 });
